330a9dfef3ede6fe7ef16e0bfcfac3e97eac7a24
otabek23 <49580505+otabek23@users.noreply.github.com>
2019-12-20 20:57:42 +0500

    Add files via upload

---
+++ 11/1/Project1.dpr
@@ -0,0 +1,43 @@
+program Project1;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.Math, System.SysUtils;
+
+var
+stoimost : array[1..20] of Integer;
+min,max,i,j,k: Integer;
+s:string;
+
+begin
+Randomize;
+for i := 1 to 20 do begin
+  stoimost[i] := RandomRange(15,75) * 1000;
+  s := s+IntToStr(stoimost[i])+' ';
+
+end;
+
+Writeln('Стоимость конфет: ',s);
+
+
+max := stoimost[1];
+min := stoimost[1];
+
+for i := 1 to 20 do begin
+  if max<stoimost[i] then begin
+    max:=stoimost[i];
+  end;
+
+   if min>stoimost[i] then begin
+    min := stoimost[i];
+  end;
+
+end;
+Writeln('Минимальная стоимость: ', min);
+Writeln('Максимальная стоимость: ', max);
+
+Readln;
+end.
