68e815bf53b50a0a190c1bfe5b96048cfb0fae87
otabek23 <49580505+otabek23@users.noreply.github.com>
2019-12-28 20:29:25 +0500

    Add files via upload

---
+++ Xushvakt 13/lab_7/n1.dpr
@@ -0,0 +1,26 @@
+program n1;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+var
+chislo,a,b,c:integer;
+begin
+  write('Введите двузначное число: ');
+  Readln(chislo);
+  b:= chislo mod 10;
+  a:= chislo div 10;
+  c:=a+b;
+  if c>=10 then begin
+    writeln('Сумма цифр числа ',chislo,' является двузначным (',c,')');
+  end else writeln('Сумма цифр числа ',chislo,' является однозначным (',c,')');
+
+  if c>chislo then begin
+    writeln('Сумма цифр числа ',chislo,' больше самого числа (',c,'>',chislo,')');
+  end else writeln('Сумма цифр числа ',chislo,' меньше самого числа (',c,'<',chislo,')');
+
+  readln;
+end.
