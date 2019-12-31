7b52b5accece0dc91174c08a57165eea9ff32403
otabek23 <49580505+otabek23@users.noreply.github.com>
2019-12-31 23:06:48 +0500

    Add files via upload

---
+++ lab_12/n1.dpr
@@ -0,0 +1,34 @@
+program n1;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils,System.Math;
+var
+naseleniye:array[1..28] of integer;
+ploshad:array[1..28] of integer;
+i,j,k:integer;
+s,ss:string;
+begin
+Randomize;
+j:=0;
+for i := 1 to 28 do begin
+  naseleniye[i]:= RandomRange(1000000,10000000);
+  ploshad[i]:=RandomRange(500000,15000000);
+  ss:= ss+ IntToStr(ploshad[i]) +' ';
+  s:= s+ IntToStr(naseleniye[i]) +' ';
+  if ploshad[i]>5000000 then begin
+    j:=naseleniye[i] + j;
+  end;
+
+end;
+
+Writeln('Население 28 государств: ',s);
+Writeln('Площадь 28 государств: ',ss);
+Writeln;
+writeln('Общая численность государств, чья площадь превышает 5 млн кв. км: ',j);
+
+Readln;
+end.
